8fabb0fdbd275fafddc4435794fac1683497235d
danielbwu <1danielbwu1@gmail.com>
2018-03-27 15:37:18 -0600

    test push

--- graphics.py
+++ graphics.py
@@ -12,41 +12,44 @@ background = None
 
 screen = None
 
+
 def update():
-	global screen, background, renderables, width, height
+    global screen, background, renderables, width, height
 
-	screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
 
-	if background:
-		screen.blit(background, (0, 0, width, height))
+    if background:
+        screen.blit(background, (0, 0, width, height))
 
-	for r in renderables:
-		r.render(screen)
+    for r in renderables:
+        r.render(screen)
 
-	pygame.display.flip()
+    pygame.display.flip()
+
 
 def register(renderable):
-	global renderables
-	if renderable not in renderables:
-		renderables.append(renderable)
+    global renderables
+    if renderable not in renderables:
+        renderables.append(renderable)
+
 
 def remove(renderable):
-	global renderables
-	if renderable in renderables:
-		renderables.remove(renderable)
+    global renderables
+    if renderable in renderables:
+        renderables.remove(renderable)
 
 
 def init():
-	global screen
-	pygame.display.init()
-	screen = pygame.display.set_mode((width, height))
+    global screen
+    pygame.display.init()
+    screen = pygame.display.set_mode((width, height))
 
 
 def load(file):
-	global assets
-	if file in assets:
-		return assets[file]
-	else:
-		image = pygame.image.load(file)
-		assets[file] = image
-		return image
+    global assets
+    if file in assets:
+        return assets[file]
+    else:
+        image = pygame.image.load(file)
+        assets[file] = image
+        return image
